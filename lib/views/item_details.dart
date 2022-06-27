267dfcde619cf76e4fc18ef8a9c0ee4ffe5d0cf1
adnan morshed <adnan.starit118@gmail.com>
2022-06-27 12:53:33 +0600

    item details 100% done

--- lib/views/item_details.dart
+++ lib/views/item_details.dart
@@ -5,8 +5,7 @@ import 'package:flutter/material.dart';
 class ItemDetails extends StatefulWidget {
 
   var users;
-
-
+  
   ItemDetails(this.users);
 
 
@@ -21,13 +20,79 @@ class _ItemDetailsState extends State<ItemDetails> {
       appBar: AppBar(
         title:  Text(widget.users['author']),
       ),
-      body: Column(
-        children: [
-          Text('width :'+widget.users['width'].toString()),
-          Text('height :'+widget.users['height'].toString()),
-          Text('url :'+widget.users['url'].toString()),
-          Text('download_url :'+widget.users['download_url'].toString()),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+
+            Card(
+              child: Container(
+                  height: 100,
+                  width: double.infinity,
+                  margin: EdgeInsets.all(20),
+                  
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    
+                    children: [
+                      Text('Book Information',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                       Divider(height: 1,thickness: 1,),
+                      Text('Author : '+ widget.users['author'].toString()),
+                      Text('Width :'+widget.users['width'].toString()),
+                      Text('Height :'+widget.users['height'].toString()),
+
+                    ],
+                  )),
+            ),
+            SizedBox(height: 10,),
+            Card(
+              child: Container(
+                  height: 100,
+                  width: double.infinity,
+
+                  child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('URL',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                      Text(widget.users['url'].toString()),
+                      TextButton(
+                        child: Text('Clickable Here'),onPressed: (){
+                        /// Clickable Here
+                      },)
+
+                    ],
+                  )),
+            ),
+
+            SizedBox(height: 10,),
+            Card(
+              child: Container(
+                  height: 100,
+                  width: double.infinity,
+
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('DownLoad ',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
+                      Text(widget.users['download_url'].toString()),
+                      TextButton(
+                        onPressed: (){},
+                        child:
+                        Icon(Icons.arrow_circle_down_outlined)
+                       )
+
+                    ],
+                  )),
+            ),
+
+            SizedBox(height: 10,),
+          ],
+        ),
       ),
 
     );
